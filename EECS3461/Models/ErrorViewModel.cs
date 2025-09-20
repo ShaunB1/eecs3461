db424dd0b6883b831e9c088f5738c1642bef81a4
Shaun Bautista <shaunbautista1@gmail.com>
2025-09-20 11:52:26 -0400

    added current work

--- EECS3461/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EECS3461.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- EECS3461/Program.cs
+++ EECS3461/Program.cs
@@ -1,17 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -20,8 +11,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
